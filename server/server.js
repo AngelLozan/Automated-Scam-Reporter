9df293f1f1f8c12b0f4636f88a6d81c2d6836309
Scott Lozano <lozans89@gmail.com>
2023-02-02 14:10:32 +0000

    updates headers

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-      res.setHeader("Access-Control-Allow-Origin", "https://scamreporterfront.onrender.com/*");
+      res.setHeader("Access-Control-Allow-Origin", "https://scamreporterfront.onrender.com");
       res.setHeader(
         "Access-Control-Allow-Methods",
         "GET, POST"
