af77cd026d18d9c82ebd2e39fec47fd9770c7b75
Scott Lozano <lozans89@gmail.com>
2023-02-02 14:02:58 +0000

    updates server

--- server/server.js
+++ server/server.js
@@ -17,17 +17,14 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-app.use((req, res) => {
+app.use((req, res, next) => {
       res.setHeader("Access-Control-Allow-Origin", "https://scamreporterfront.onrender.com/*");
       res.setHeader(
         "Access-Control-Allow-Methods",
-        "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+        "GET, POST"
       );
-      res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-      if (req.method === "OPTIONS") {
-        return res.sendStatus(200);
-      }
-     
+      res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+      next();
     });
 
 app.get("/", (req, res) => {
