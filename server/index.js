ca28d75e7507d8404016a3ba26de5026c8726e10
Scott Lozano <lozans89@gmail.com>
2023-02-02 12:06:51 +0000

    updates server

--- server/index.js
+++ server/index.js
@@ -12,10 +12,10 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join('client/build')));
 
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join('client/build', 'index.html'));
   });
 }
 
