652f2fd2e8fc01b249e1b49b5a9708189483abff
Scott Lozano <lozans89@gmail.com>
2023-02-07 15:56:08 +0000

    changes server headers

--- server/server.js
+++ server/server.js
@@ -29,8 +29,8 @@ const client = new OAuth2Client(GOOGLE_CLIENT_ID);
 let DB = [];
 
 app.use((req, res, next) => {
-      res.setHeader("Access-Control-Allow-Origin", "https://scamreporterfront.onrender.com"); //@dev "https://scamreporterfront.onrender.com" For local: "http://localhost:3000/"
-      res.setHeader(
+      res.header("Access-Control-Allow-Origin", "https://scamreporterfront.onrender.com"); //@dev "https://scamreporterfront.onrender.com" For local: "http://localhost:3000/"
+      res.header(
         "Access-Control-Allow-Methods",
         "GET,POST,PUT,DELETE,OPTIONS"
       );
