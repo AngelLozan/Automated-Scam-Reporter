c623574859cbdd730a5016e74196461e445c1c8d
Scott Lozano <lozans89@gmail.com>
2023-02-09 15:57:50 +0000

    updates language and tiktok ready api

--- client/src/screens/TikTok.jsx
+++ client/src/screens/TikTok.jsx
@@ -85,7 +85,7 @@ const TikTok = ({user}) => {
         <img src={logo} className={loading ? 'App-logo-spin' : 'App-logo'} alt="logo" />
       </header>
       <h2>Tik Tok</h2>
-      <p>This will take one parameter, a TikTok Profile URL and fill the TradeMark report against it.</p>
+      <p>This action takes one parameter, a TikTok Profile URL and fills the TradeMark report against it.</p>
       <p>{!ready ? "Server not ready yet" : ready}</p>
 
       <form onSubmit={handleSubmit}>
